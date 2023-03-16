4646583d88f32aa80ae3b4d91ddd05108b150fbc
Nikita Panferov <nikpanfer@gmail.com>
2023-03-17 00:24:31 +0300

    Done Homework_7

--- frontend/src/pages/login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {LoginForm} from "../components/login_form";
-export const Login = (props) => {
-    return (
-        <div>
-            <LoginForm getToken={props.getToken}/>
-        </div>
-    );
-};
